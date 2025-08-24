94d7e136761258091b8f457ac4d8caf066cc5fd9
pulkit-0715 <singhpulkit1506@gmail.com>
2025-08-25 04:38:40 +0530

    Add files via upload

---
+++ Question1-10/q01.c
@@ -0,0 +1,34 @@
+/*
+Q1: Write a program to input two numbers and display their sum.
+
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+*/
+
+/*
+Subbmition by Pulkit Singh
+Btech CS
+5990022181
+Batch - 12
+*/
+
+#include <stdio.h>
+
+int main()
+{
+   double a, b;
+   printf("Enter two numbers\n");
+    scanf("%lf %lf", &a, &b);
+
+    printf("Sum = %lf\n", a + b);
+
+    return 0;
+}
