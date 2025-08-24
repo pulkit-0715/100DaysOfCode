e4b657496aebb14e3c6f9fb5c5539c5f0f0afa51
pulkit-0715 <singhpulkit1506@gmail.com>
2025-08-25 04:23:48 +0530

    Add files via upload

---
+++ Question1-10/q5.c
@@ -0,0 +1,33 @@
+/*
+Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/
+
+/*
+Subbmition by Pulkit Singh
+Btech CS
+5990022181
+Batch - 12
+*/
+
+#include <stdio.h>
+
+int main(){
+    double c;
+    printf("Enter temprature in celsius\n");
+    scanf("%lf",&c);
+    printf("Fahrenheit = %.2lf" ,((9.0/5) * c) + 32);
+
+    return 0;
+}
---
+++ Question1-10/q6.c
@@ -0,0 +1,38 @@
+/*
+Q6: Write a program to swap two numbers using a third variable.
+
+Sample Test Cases:
+Input 1:
+3 5
+Output 1:
+After swap: 5 3
+
+Input 2:
+-1 1
+Output 2:
+After swap: 1 -1
+
+*/
+
+/*
+Subbmition by Pulkit Singh
+Btech CS
+5990022181
+Batch - 12
+*/
+#include <stdio.h>
+
+int main(){
+    int n1,n2;
+    printf("Enter 2 number");
+    scanf("%d %d",&n1,&n2);
+    printf("n1= %d\n",n1);
+    printf("n2= %d\n",n2);
+    int n3=n1;
+    n1=n2;
+    n2=n3;
+    printf("After Swap: %d %d",n1,n2);
+
+
+    return 0;
+}
