04ebb613b7bdf6371b865e735552ec12c55ebef2
pulkit-0715 <singhpulkit1506@gmail.com>
2025-09-03 16:07:38 +0530

    Update q24.c

--- Question21-30/q24.c
+++ Question21-30/q24.c
@@ -1,4 +1,9 @@
-/*Q24: Write a program to calculate an electricity bill based on units consumed.
+/*
+Q24: Write a program to calculate electricity bill based on units consumed with these rates: 
+First 100 units at ₹5/unit 
+Next 100 units at ₹7/unit 
+Next 100 units at ₹10/unit 
+Above at ₹12/unit
 
 Sample Test Cases:
 Input 1:
@@ -9,12 +14,13 @@ Bill: ₹250
 Input 2:
 150
 Output 2:
-Bill: ₹950
+Bill: ₹850
 
 Input 3:
 250
 Output 3:
-Bill: ₹2200
+Bill: ₹1700
+
 
 Subbmition by Pulkit Singh
 Btech CS
