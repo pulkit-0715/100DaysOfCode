41d0bd93b0137b8e0a6b848463c22d4d2a5fec50
Pulkit Singh <singhpulkit1506@gmail.com>
2025-09-08 00:36:37 +0530

    Add files via upload

---
+++ Question31-40/q38.c
@@ -0,0 +1,51 @@
+/*Q38: Write a program to find the sum of digits of a number.
+
+Sample Test Cases:
+Input 1:
+123
+Output 1:
+6
+
+Input 2:
+999
+Output 2:
+27
+
+
+Subbmition by Pulkit Singh
+Btech CS
+590022181
+Batch - 12
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    long long int n,ans=1;
+    printf("Enter a number\n");
+    scanf("%lld",&n);
+    long long int temp=n;
+
+    if (n==0)
+    {
+        printf("Ans: 0");
+        return 0;
+    }
+    
+    while (temp!=0)
+    {
+         if (temp%10==0)
+      {
+        printf("Ans: 0");
+        return 0;
+      }
+        ans*=(temp%10);
+        temp/=10;
+    }
+
+
+    printf("Ans: %lld",ans);
+
+     return ans;
+}
