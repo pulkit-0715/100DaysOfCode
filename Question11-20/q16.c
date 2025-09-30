ee3469df6ca55c04e179e5f6139ee9cc43f67582
Pulkit Singh <singhpulkit1506@gmail.com>
2025-09-30 13:18:22 +0530

    Update q16.c

--- Question11-20/q16.c
+++ Question11-20/q16.c
@@ -22,7 +22,7 @@ int main(){
     int a,b,c;
     printf("Enter 3 number");
     scanf("%d %d %d",&a,&b,&c);
-    if(a>b&&a>b)
+    if(a>b&&a>c)
     printf("Largest is %d",a);
     else if(b>a&&b>c)
     printf("Largest is %d",b);
